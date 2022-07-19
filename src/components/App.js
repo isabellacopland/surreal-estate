ede646d1bcb549f3d321ef15a979233531b59c60
isabellacopland <isabella.copland@gmail.com>
2022-07-19 21:58:48 +0100

    set up

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import '../styles/app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Surreal Estate
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
